c8ca9e6adc6ecd87232ac94195cb61c31e6c4bbc
Blake14 <blakemcpherson2@gmail.com>
2025-02-06 09:48:31 -0600

    build: add functionality to add result to clipboard

---
+++ LogikAdmin/debug-string-builder.ps1
@@ -0,0 +1,21 @@
+$logikInputs = @"
+let ConditionValueA = cfg.standardSelectionAccessoryType;
+let ConditionValueB = cfg.standardSelectionInletSize;
+"@
+
+$cfg = @{}
+
+foreach ($line in $logikInputs -split "`n") {
+    if ($line -match 'cfg\.(\w+);') {
+        $key = $matches[1]
+        $cfg[$key] = ""
+    }
+}
+
+$json = @{ "cfg" = $cfg } | ConvertTo-Json -Depth 10
+
+Write-Output $json
+$json | Set-Clipboard
+
+Write-Output "JSON also copied to clipboard:"
+Write-Output $json
