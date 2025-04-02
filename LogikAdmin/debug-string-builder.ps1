c3f8838fbc88c83760410d5f562aca3852c42c88
Blake14 <blakemcpherson2@gmail.com>
2025-04-02 15:59:57 -0500

    bug: adjustments to debugger

--- LogikAdmin/debug-string-builder.ps1
+++ LogikAdmin/debug-string-builder.ps1
@@ -1,6 +1,70 @@
 $logikInputs = @"
-let ConditionValueA = cfg.standardSelectionAccessoryType;
-let ConditionValueB = cfg.standardSelectionInletSize;
+let ProductType = cfg.standardSelectionProductType;
+let Grade = cfg.standardSelectionGrade;
+let Series = cfg.standardSelectionSilencerSeries;
+let Material = cfg.standardSelectionMaterial;
+let Geometry = cfg.standardSelectionGeometry;
+let Performance = cfg.standardSelectionApexPerformance;
+let Application = cfg.standardSelectionApexApplication;
+let InletSize = cfg.standardSelectionInletSize;
+let OutletSize = cfg.standardSelectionOutletSize;
+let ProductSubcategory = cfg.standardSelectionProductCategory;
+let InletQty = cfg.standardSelectionInletQuantity;
+let Orientation = cfg.standardSelectionp4silencerOrientationName;
+let OutletOrientation = cfg.standardSelectionp4OutletOrientation;
+let InletOrientation = cfg.standardSelectionProduct40SilencerConfigurationInletOrientation;
+let CylType = cfg.standardSelectionCylindricalType;
+let AccessoryType = cfg.standardSelectionAccessoryType;
+let InletConnection = cfg.standardSelectionInletConnection;
+let InletFlangeType = cfg.standardSelectionInletFlangeMountType;
+let OutletConnection = cfg.standardSelectionOutletConnection;
+let OutletFlangeType = cfg.standardSelectionOutletFlangeMountType;
+let Insulation = cfg.standardSelectionInsulation;
+let MountType = cfg.standardSelectionMountType;
+let ApexDiameter = cfg.standardSelectionRoundAPXDia;
+let ApexLength = cfg.standardSelectionSquareAPXLength;
+let ApexWidth = cfg.standardSelectionSquareAPXWidth;
+let ApexDimTog = cfg.standardSelectionAPEXDimensionsToggle;
+let LengthDependentCosting = cfg.standardSelectionLengthDependentCosting;
+
+let PipeOrTube = cfg.standardSelectionPipeOrTube;
+let OverallLength = cfg.standardSelectionOverallLength;
+let NominalDiameter = cfg.standardSelectionNominalDiameter;
+let Style = cfg.standardSelectionP4Style;
+let WyeLayout = cfg.standardSelectionProduct40AccWyeLayout;
+
+let InletSpacing = cfg.standardSelectionP4OffsetX;
+let FlangeType = cfg.standardSelectionP4FlangeType;
+let FlangeGroup = cfg.standardSelectionP4FlangeGroup;
+let FlangePrefix = cfg.standardSelectionP4AccFlangePrefix;
+let ElbowType = cfg.standardSelectionP4ElbowType;
+let ElbowInput = cfg.standardSelectionP4FlexElbowInput;
+let FlexSize = cfg.standardSelectionP4FlexSizeInput;
+let InletCenterlineToOutletFace = cfg.standardSelectionP4InletCenterlineToOutletFace;
+let InletFaceToOutletCenterline = cfg.standardSelectionP4InletFaceToOutletCenterline;
+let LegLength1 = cfg.standardSelectionLegLengthInlet;
+let LegLength2 = cfg.standardSelectionLegLengthOutlet;
+let LengthOffset = cfg.standardSelectionP4OffsetZ;
+let OuterDiameter1 = cfg.standardSelectionOuterDiameter1;
+let OuterDiameter2 = cfg.standardSelectionP4OuterDiameter2;
+let OuterDiameter3 = cfg.standardSelectionP4OuterDiameter3;
+let AccWidth = cfg.standardSelectionP4Width;
+let AccWeight = cfg.standardSelectionP4Weight;
+//let AccHeight = cfg.standardSelectionP4Height;
+let AccMaxSize = cfg.standardSelectionP4MaxSize;
+let AccMinSize = cfg.standardSelectionP4MinSize;
+let BoltCircleDia = cfg.standardSelectionP4BoltCircleDiameter;
+//let BoltHoleDia = cfg.
+let BoltSize = cfg.standardSelectionP4BoltSize;
+let BoltSpacing = cfg.standardSelectionP4BoltSpacing;
+let Flare = cfg.standardSelectionP4Flare;
+let InnerDiameter = cfg.standardSelectionP4InnerDiameter;
+let BoltHoleQty = cfg.standardSelectionP4BoltHoleQty;
+let WallThickness = cfg.standardSelectionP4WallThickness;
+let Thickness1 = cfg.standardSelectionP4Thickness1;
+let Thickness2 = cfg.standardSelectionP4Thickness2;
+
+let PartNumberSearch = cfg.standardSelectionPartNumberSearch;
 "@
 
 $cfg = @{}
@@ -14,9 +78,7 @@ foreach ($line in $logikInputs -split "`n") {
 
 $json = @{ "cfg" = $cfg } | ConvertTo-Json -Depth 10
 
-Write-Output $json
 $json | Set-Clipboard
 
-Write-Output "JSON also copied to clipboard:"
 Write-Output $json
-
+Write-Output "JSON also copied to clipboard:"
